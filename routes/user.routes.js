e14d1633924258ff0372e40372571946f684e188
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-07-23 08:00:17 +0300

    feat: add response status constants and integrate CORS middleware
    - Created a new constants file for response statuses.
    - Integrated CORS middleware into the main application for handling cross-origin requests.
    - Updated user model to include email validation and additional fields.
    - Added user registration route in user routes file.
    - Updated package dependencies to include cors and express-validator.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,14 @@ const express = require("express");
 const userRouter = express.Router();
 const responseStatus = require("../constants/response.status");
 userRouter.post("/api/users/register", (req, res) => {
-  const date = req.body;
+  const userdata = req.body;
 });
 
 //get all Users
 // auth (Registar,signIn)
+/*
+{
+ username : 
+} 
+
+*/
